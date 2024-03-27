ca4a87a8b5c791c5231e45927148c7634819ad1d
Tanmay Bhatgare <tanmaybhatgare07@gmail.com>
2024-03-27 19:00:30 +0530

    Added Toggle Password

---
+++ script.js
@@ -0,0 +1,10 @@
+let passwordInput = document.getElementById('password-input');
+let passwordCheckbox = document.getElementById('checkbox');
+
+passwordCheckbox.addEventListener('change', ()=>{
+    if(passwordCheckbox.checked){
+        passwordInput.type = 'text';
+    }else{
+        passwordInput.type = 'password';
+    }
+})
